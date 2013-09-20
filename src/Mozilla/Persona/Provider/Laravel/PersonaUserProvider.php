bdb7a9adae094ec9c16a81065394f3550baeabe5
Jonas Stendahl <jonas.stendahl@gmail.com>
2013-09-20 14:03:43 +0200

    Fixed issues with Laravel's PersonaUserProvider.

--- src/Mozilla/Persona/Provider/Laravel/PersonaUserProvider.php
+++ src/Mozilla/Persona/Provider/Laravel/PersonaUserProvider.php
@@ -2,6 +2,8 @@
 namespace Mozilla\Persona\Provider\Laravel;
 
 use App;
+use Config;
+use DB;
 use Exception;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\UserProviderInterface;
@@ -9,15 +11,40 @@ use Illuminate\Auth\UserProviderInterface;
 class PersonaUserProvider implements UserProviderInterface
 {
 
+    const EVENT_LOGIN    = 'persona.login';
+    const EVENT_REGISTER = 'persona.register';
+
     protected $identity;
 
-    public function retrieveById($identifier)
+    public function retrieveById($email)
     {
-        dd('retrieveById', $identifier);
+
+        $event = App::make('events');
+
+        if ($event->hasListeners(self::EVENT_LOGIN)) {
+
+            $user = $event->fire(self::EVENT_LOGIN, array($email));
+            $user = $user[0];
+
+            if (($user instanceof UserInterface) === false and $user !== null) {
+                $type = (is_object($user[0])) ? get_class($user[0]) : gettype($user[0]);
+                throw new Exception(
+                    self::EVENT_LOGIN.' expected an instance of Illuminate\\Auth\\UserInterface, '.$type.' received.'
+                );
+            }
+
+        } else {
+            $user = DB::table(Config::get('auth.table'))->where('email', $email)->first();
+            $user = ($user !== null) ? new PersonaUser((array) $user) : $user;
+        }
+
+        return $user;
+
     }
 
     public function retrieveByCredentials(array $credentials)
     {
+
         if (isset($credentials['assertion']) === false) {
             throw new Exception('Missing assertion.');
         }
@@ -29,22 +56,15 @@ class PersonaUserProvider implements UserProviderInterface
 
         if ($identity->isValid()) {
 
-            $user = App::make('events')->fire('persona.login', array($identity));
+            $user = $this->retrieveById($identity->getEmail());
 
-            if (isset($user[0]) === false) {
-                throw new Exception(
-                    'You must register an observer of the event "persona.login" to use Mozilla Persona authentication.'
-                );
-            } elseif (($user[0] instanceof UserInterface) === false) {
-                $type = (is_object($user[0])) ? get_class($user[0]) : gettype($user[0]);
-                throw new Exception(
-                    '"persona.login" expected an instance of Illuminate\\Auth\\UserInterface, '.$type.' received.'
-                );
+            if ($user === null) {
+                $user = $this->createUser($identity->getEmail());
             }
 
             $this->identity = $identity;
 
-            return $user[0];
+            return $user;
 
         }
 
@@ -55,5 +75,31 @@ class PersonaUserProvider implements UserProviderInterface
     public function validateCredentials(UserInterface $user, array $credentials)
     {
         return ($user->getAuthIdentifier() === $this->identity->getEmail());
+    }
+
+    protected function createUser($email)
+    {
+
+        $event = App::make('events');
+
+        if ($event->hasListeners(self::EVENT_REGISTER)) {
+
+            $user = $event->fire(self::EVENT_REGISTER, array($email));
+            $user = $user[0];
+
+            if (($user instanceof UserInterface) === false and $user !== null) {
+                $type = (is_object($user[0])) ? get_class($user[0]) : gettype($user[0]);
+                throw new Exception(
+                    self::EVENT_LOGIN.' expected an instance of Illuminate\\Auth\\UserInterface, '.$type.' received.'
+                );
+            }
+
+        } else {
+            DB::table(Config::get('auth.table'))->insert(array('email' => $email));
+            $user = $this->retrieveById($email);
+        }
+
+        return $user;
+
     }
 }
